65665b7ab405e55776ce90a0af5b75ff065d82f1
Deepak Guptha <vzan2012@gmail.com>
2024-04-24 22:35:10 -0400

    build: modified vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/vzan2012.github.io/",
   build: {
     outDir: "dist",
   },
