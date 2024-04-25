825d449575a8410f2d115aea210bd4e06d083677
Deepak Guptha <vzan2012@gmail.com>
2024-04-25 10:29:57 -0400

    fix: folder structure and workflows

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vzan2012.github.io/",
+  // base: "/",
   build: {
     outDir: "dist",
   },
