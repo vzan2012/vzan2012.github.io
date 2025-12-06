c5f5bf53adaf418f3741daf7743c66650cfe7d10
Deepak Guptha <vzan2012@gmail.com>
2025-12-06 10:02:30 -0500

    Revert "feat: updated biome config for lint"
    This reverts commit f818f2d702db9e132f24a4a3ff4d9b9d16960cfd.

--- public/js/resume.js
+++ public/js/resume.js
@@ -1,60 +1,64 @@
-(($) => {
-	// Smooth scrolling using jQuery easing
-	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-		if (
-			location.pathname.replace(/^\//, "") ==
-				this.pathname.replace(/^\//, "") &&
-			location.hostname == this.hostname
-		) {
-			var target = $(this.hash);
-			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-			if (target.length) {
-				$("html, body").animate(
-					{
-						scrollTop: target.offset().top,
-					},
-					1000,
-					"easeInOutExpo",
-				);
-				return false;
-			}
-		}
-	});
+(function ($) {
+  "use strict"; // Start of use strict
 
-	// Closes responsive menu when a scroll trigger link is clicked
-	$(".js-scroll-trigger").click(() => {
-		$(".navbar-collapse").collapse("hide");
-	});
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
+      }
+    }
+  });
 
-	// Activate scrollspy to add active class to navbar items on scroll
-	$("body").scrollspy({
-		target: "#sideNav",
-	});
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
 
-	// Disable Right click in the page
-	$("#page-top").on("contextmenu", (e) => false);
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#sideNav",
+  });
 
-	// Enable Tooltips
-	$('[data-toggle="tooltip"]').tooltip();
+  // Disable Right click in the page
+  $("#page-top").on("contextmenu", function (e) {
+    return false;
+  });
 
-	// Magnific Popup - Project Section Scripts
-	$(".image-link").magnificPopup({
-		type: "image",
-		gallery: {
-			enabled: false,
-		},
-		callbacks: {
-			open: function () {
-				const textContent = $(this.currItem.el)
-					.siblings(".text-container")
-					.clone();
-				console.log();
-				this.content.append(textContent);
-				textContent.show();
-			},
-			close: function () {
-				this.content.find(".text-container").remove();
-			},
-		},
-	});
+  // Enable Tooltips
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Magnific Popup - Project Section Scripts
+  $(".image-link").magnificPopup({
+    type: "image",
+    gallery: {
+      enabled: false,
+    },
+    callbacks: {
+      open: function () {
+        let textContent = $(this.currItem.el)
+          .siblings(".text-container")
+          .clone();
+        console.log();
+        this.content.append(textContent);
+        textContent.show();
+      },
+      close: function () {
+        this.content.find(".text-container").remove();
+      },
+    },
+  });
 })(jQuery); // End of use strict
